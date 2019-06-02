2299b89895d445dd9bbb9a52572fb5192efd1374
rocketgithub <rodrigo@solucionesprisma.com>
2019-06-02 10:36:54 -0600

    Dejar editable la sesión

--- models/sale.py
+++ models/sale.py
@@ -7,5 +7,4 @@ class SaleOrder(models.Model):
     def _default_sesion(self):
         return self.env['sesion.ventas'].search([('estado', '=', 'abierto'), ('usuarios_ids', 'in', [self.env.uid])], limit=1)
 
-    sesion_ventas_id = fields.Many2one("sesion.ventas",string="Session",domain="[('estado', '=', 'abierto')]",
-        readonly=True, default=_default_sesion)
+    sesion_ventas_id = fields.Many2one("sesion.ventas", string="Session", domain="[('estado', '=', 'abierto')]", default=_default_sesion)
